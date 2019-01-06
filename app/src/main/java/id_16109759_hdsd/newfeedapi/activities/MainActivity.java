87bb6533c61a3c84e10b63e644c628d07db1c5d4
SeanHeaslip <29168888+heaslis3-dcu@users.noreply.github.com>
2019-01-06 20:52:42 +0000

    Basic structure
    Added File/Directory basic structure

---
+++ app/src/main/java/id_16109759_hdsd/newfeedapi/activities/MainActivity.java
@@ -0,0 +1,17 @@
+package id_16109759_hdsd.newfeedapi.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import id_16109759_hdsd.newfeedapi.R;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
